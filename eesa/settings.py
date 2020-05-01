9fa462cc16b5a64232ded4e2f78a876635291bc9
Soroush M Pour <soroushmpour@gmail.com>
2020-05-01 23:29:29 +0430

    Update settings.py

--- eesa/settings.py
+++ eesa/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qr!bu=gg^+g1l1_3znt6o6hfg@1k+2$l8mo-a_3s+6^7&_ncat'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
